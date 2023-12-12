5f453c2b19174fafffbafd6ab892a0e573c56b51
Calum <contact@mineblock11.dev>
2023-12-12 20:17:38 +0000

    fix: THROW MORE THREADS AT IT?!?

--- src/main/java/dev/imb11/sounds/mixin/ui/ImageRenderManagerMixin.java
+++ src/main/java/dev/imb11/sounds/mixin/ui/ImageRenderManagerMixin.java
@@ -1,14 +1,12 @@
 package dev.imb11.sounds.mixin.ui;
 
 import dev.isxander.yacl3.gui.image.ImageRendererManager;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Pseudo
 @Mixin(ImageRendererManager.class)
 public class ImageRenderManagerMixin {
     @Mutable

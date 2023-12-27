cad3448b7f4bfb577beaf36c39fc559317d537eb
MUKSC <68376185+MUKSC@users.noreply.github.com>
2023-12-27 14:29:27 +0900

    refactor: ItemTransferSoundEffects

--- src/main/java/dev/imb11/sounds/mixin/ui/ItemTransferSoundEffects.java
+++ src/main/java/dev/imb11/sounds/mixin/ui/ItemTransferSoundEffects.java
@@ -1,72 +1,52 @@
 package dev.imb11.sounds.mixin.ui;
 
-import dev.imb11.sounds.SoundsClient;
 import dev.imb11.sounds.config.UISoundConfig;
 import dev.imb11.sounds.dynamic.DynamicSoundHelper;
 import dev.imb11.sounds.sound.context.ItemStackSoundContext;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ConcurrentModificationException;
-
 @Mixin(ScreenHandler.class)
 public abstract class ItemTransferSoundEffects {
+    @Shadow public abstract ItemStack getCursorStack();
+
     @Shadow public abstract Slot getSlot(int index);
-    @Unique
-    private double prevTime = 0D;
 
-    @Inject( method = "method_34249", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/screen/ScreenHandler;setCursorStack(Lnet/minecraft/item/ItemStack;)V"))
-    void $item_click_0_sound_effect(Slot slot, PlayerEntity playerEntity, ItemStack stack, CallbackInfo ci)
-    {
-        double currentTime = GLFW.glfwGetTime();
-        double timeElapsed = currentTime - prevTime;
+    @Inject(method = "internalOnSlotClick", at = @At("HEAD"))
+    void $item_transfer_sound_effects(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+        if (!(player instanceof ClientPlayerEntity)) return;
+        if (slotIndex < 0) return;
 
-        if (timeElapsed >= 0.09D) {
-            if (!stack.isEmpty()) {
-                try {
-                    UISoundConfig.get().itemClickSoundEffect.playDynamicSound(stack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
-                } catch (ConcurrentModificationException ignored) {
-                    SoundsClient.LOGGER.warn("Captured ConcurrentModificationException in ItemTransferSoundEffects mixin.");
-                }
+        ItemStack itemStack = getSlot(slotIndex).getStack();
+        if (actionType == SlotActionType.PICKUP) {
+            if (itemStack.isEmpty()) {
+                ItemStack cursorStack = this.getCursorStack();
+                UISoundConfig.get().itemClickSoundEffect.playDynamicSound(cursorStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
+            } else {
+                UISoundConfig.get().itemClickSoundEffect.playDynamicSound(itemStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
             }
-
-            prevTime = currentTime;
-        }
-    }
-
-    @Inject(method = "internalOnSlotClick", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/screen/ScreenHandler;setCursorStack(Lnet/minecraft/item/ItemStack;)V"))
-    void $item_click_1_sound_effect(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci)
-    {
-        if (slotIndex >= 0)
-            UISoundConfig.get().itemClickSoundEffect.playDynamicSound(getSlot(slotIndex).getStack(), ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
-    }
-
-    @Inject(method = "internalOnSlotClick", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/item/ItemStack;increment(I)V"))
-    void $item_transfer_0_sound_effects(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci)
-    {
-        double currentTime = GLFW.glfwGetTime();
-        double timeElapsed = currentTime - prevTime;
-
-        if (timeElapsed >= 0.09D) {
-            if (slotIndex >= 0) {
-                try {
-                    UISoundConfig.get().itemClickSoundEffect.playDynamicSound(getSlot(slotIndex).getStack(), ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
-                } catch (ConcurrentModificationException ignored) {
-                    SoundsClient.LOGGER.warn("Captured ConcurrentModificationException in ItemTransferSoundEffects mixin.");
-                }
+        } else if (actionType == SlotActionType.PICKUP_ALL) {
+            UISoundConfig.get().itemClickSoundEffect.playSound();
+        } else if (actionType == SlotActionType.SWAP) {
+            if (itemStack.isEmpty()) {
+                ItemStack buttonStack = player.getInventory().getStack(button);
+                UISoundConfig.get().itemClickSoundEffect.playDynamicSound(buttonStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
+            } else {
+                UISoundConfig.get().itemClickSoundEffect.playDynamicSound(itemStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
             }
-
-            prevTime = currentTime;
+        } else if (actionType == SlotActionType.QUICK_MOVE) {
+            UISoundConfig.get().itemClickSoundEffect.playDynamicSound(itemStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
+        } else if (actionType == SlotActionType.CLONE) {
+            UISoundConfig.get().itemCopySoundEffect.playDynamicSound(itemStack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
         }
     }
 }

6200465bf010bd38c5e5007ec32b51fec015dd1d
Calum <contact@mineblock11.dev>
2024-05-24 22:07:38 +0100

    fix: Remove YACL reload listener, not needed anymore.

--- src/main/java/dev/imb11/sounds/SoundsClient.java
+++ src/main/java/dev/imb11/sounds/SoundsClient.java
@@ -3,7 +3,6 @@ package dev.imb11.sounds;
 import dev.imb11.sounds.config.SoundsConfig;
 import dev.imb11.sounds.dynamic.DynamicSoundHelper;
 import dev.imb11.sounds.dynamic.SoundsReloadListener;
-import dev.imb11.sounds.gui.images.YACLImageReloadListenerFabric;
 import dev.imb11.sounds.sound.events.PotionEventHelper;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -25,7 +24,6 @@ public class SoundsClient implements ClientModInitializer {
         DynamicSoundHelper.initialize();
 
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SoundsReloadListener());
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new YACLImageReloadListenerFabric());
 
         PotionEventHelper.initialize();
     }

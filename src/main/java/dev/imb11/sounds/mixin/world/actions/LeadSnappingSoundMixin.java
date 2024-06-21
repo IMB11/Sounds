00a6f58b38f8a66320dcb962c88b99cecc29db5f
Calum <contact@mineblock11.dev>
2024-06-21 21:35:16 +0100

    feat: Leash fix.

--- src/main/java/dev/imb11/sounds/mixin/world/actions/LeadSnappingSoundMixin.java
+++ src/main/java/dev/imb11/sounds/mixin/world/actions/LeadSnappingSoundMixin.java
@@ -2,17 +2,26 @@ package dev.imb11.sounds.mixin.world.actions;
 
 import dev.imb11.sounds.config.SoundsConfig;
 import dev.imb11.sounds.config.WorldSoundsConfig;
-import net.minecraft.entity.mob.MobEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MobEntity.class)
+/*? if >=1.21 {*/
+@Mixin(net.minecraft.entity.Leashable.class)
+/*? } else { *//*
+@Mixin(net.minecraft.entity.mob.MobEntity.class)
+*//*? }*/
 public class LeadSnappingSoundMixin {
+    /*? if <=1.20.6 { *//*
     @Inject(method = "detachLeash", at = @At("TAIL"), cancellable = false)
     public void $lead_snapping_sound_effect(boolean sendPacket, boolean dropItem, CallbackInfo ci) {
-        if (dropItem) {
+        if(dropItem) {
+    *//*? } else { */
+    @Inject(method = "detachLeash(Lnet/minecraft/entity/Entity;ZZ)V", at = @At("TAIL"), cancellable = false)
+    private static <E extends net.minecraft.entity.Entity & net.minecraft.entity.Leashable> void $lead_snapping_sound_effect(E entity, boolean sendPacket, boolean dropItem, CallbackInfo ci) {
+        if (dropItem && entity.getWorld().isClient) {
+    /*? }*/
             SoundsConfig.get(WorldSoundsConfig.class).leadSnappingSoundEffect.playSound();
         }
     }

6c73449587c3828845ce948f92eef160d5493cf2
Calum <hendersoncal117@gmail.com>
2025-03-11 10:36:05 +0000

    fix: Crash when clicking mod-specific options.
    Closes: #157

--- src/main/java/dev/imb11/sounds/config/ModConfig.java
+++ src/main/java/dev/imb11/sounds/config/ModConfig.java
@@ -1,10 +1,13 @@
 package dev.imb11.sounds.config;
 
 import dev.imb11.sounds.config.utils.ConfigGroup;
+import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.YetAnotherConfigLib;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+
+import static dev.imb11.sounds.config.SoundsConfig.HELPER;
 
 public class ModConfig extends ConfigGroup<ModConfig> implements YetAnotherConfigLib.ConfigBackedBuilder<ModConfig> {
     @SerialEntry
@@ -36,6 +39,9 @@ public class ModConfig extends ConfigGroup<ModConfig> implements YetAnotherConfi
 
     @Override
     public YetAnotherConfigLib.Builder build(ModConfig defaults, ModConfig config, YetAnotherConfigLib.Builder builder) {
-        return builder;
+        return builder.title(getName())
+                .category(ConfigCategory.createBuilder()
+                        .name(getName())
+                        .option(HELPER.get("hideSoundsButtonInSoundMenu", defaults.hideSoundsButtonInSoundMenu, () -> hideSoundsButtonInSoundMenu, val -> hideSoundsButtonInSoundMenu = val, false)).build());
     }
 }

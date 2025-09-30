fd5c329fb0bce4aac424645bdd2b70559eb3b019
Cassian Godsted <cassian@godsted.com>
2025-09-30 11:57:27 -0400

    fix: buildscript

---
+++ build.fabric.gradle.kts
@@ -0,0 +1,111 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("fabric-loom")
+    id("dev.kikugie.postprocess.jsonlang")
+    id("me.modmuss50.mod-publish-plugin")
+}
+
+tasks.named<ProcessResources>("processResources") {
+    fun prop(name: String) = project.property(name) as String
+
+    val props = HashMap<String, String>().apply {
+        this["mod_version"] = prop("mod.version")
+        this["target_minecraft"] = prop("deps.minecraft")
+        this["mod_id"] = "sounds"
+        this["mod_name"] = "Sounds"
+        this["mod_description"] = "A highly configurable sound overhaul mod that adds new sound effects while improving vanilla sounds too."
+        this["mod_license"] = "ARR"
+        this["target_yacl"] = "*"
+        this["target_mru"] = prop("deps.mru")
+        this["target_fabricloader"] = prop("deps.fabric_loader")
+    }
+
+    filesMatching(listOf("fabric.mod.json", "META-INF/neoforge.mods.toml", "META-INF/mods.toml")) {
+        expand(props)
+    }
+}
+
+version = "${property("mod.version")}+${property("deps.minecraft")}-fabric"
+base.archivesName = property("mod.id") as String
+
+jsonlang {
+    languageDirectories = listOf("assets/${property("mod.id")}/lang")
+    prettyPrint = true
+}
+
+repositories {
+    mavenLocal()
+    maven("https://mvn.devos.one/snapshots/")
+    maven("https://maven.wispforest.io")
+    maven("https://maven.imb11.dev/releases")
+    maven("https://maven.neoforged.net/releases/")
+    maven("https://maven.isxander.dev/releases")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://maven.quiltmc.org/repository/release")
+    maven("https://maven.shedaniel.me/")
+    maven("https://maven.terraformersmc.com/releases")
+    maven("https://maven.isxander.dev/releases")
+    maven("https://thedarkcolour.github.io/KotlinForForge/")
+    maven("https://api.modrinth.com/maven")
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${property("deps.minecraft")}")
+    mappings(loom.layered {
+        officialMojangMappings()
+        if (hasProperty("deps.parchment"))
+            parchment("org.parchmentmc.data:parchment-${property("deps.parchment")}@zip")
+    })
+    modImplementation("net.fabricmc:fabric-loader:${property("deps.fabric_loader")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("deps.fabric_api")}")
+
+    modImplementation("dev.imb11:mru:${property("deps.mru")}+fabric")
+
+    modCompileOnly("dev.emi:emi-fabric:${property("compile.emi")}")
+    modCompileOnly("maven.modrinth:trashslot:${property("compile.trashslot")}")
+
+    modImplementation("dev.isxander:yet-another-config-lib:${property("deps.yacl")}-fabric")
+    modCompileOnly("com.terraformersmc:modmenu:${property("runtime.modmenu")}")
+
+    val modules = listOf("transitive-access-wideners-v1", "registry-sync-v0", "resource-loader-v0")
+    for (it in modules) modImplementation(fabricApi.module("fabric-$it", property("deps.fabric_api") as String))
+}
+
+fabricApi {
+    configureDataGeneration() {
+        outputDirectory = file("$rootDir/src/main/generated")
+        client = true
+    }
+}
+
+tasks {
+    processResources {
+        exclude("**/neoforge.mods.toml", "**/mods.toml")
+    }
+
+    register<Copy>("buildAndCollect") {
+        group = "build"
+        from(remapJar.map { it.archiveFile })
+        into(rootProject.layout.buildDirectory.file("libs/${project.property("mod.version")}"))
+        dependsOn("build")
+    }
+}
+
+java {
+    withSourcesJar()
+    val javaCompat = if (stonecutter.eval(stonecutter.current.version, ">=1.21")) {
+        JavaVersion.VERSION_21
+    } else {
+        JavaVersion.VERSION_17
+    }
+    sourceCompatibility = javaCompat
+    targetCompatibility = javaCompat
+}
+
+val additionalVersionsStr = findProperty("publish.additionalVersions") as String?
+        val additionalVersions: List<String> = additionalVersionsStr
+        ?.split(",")
+        ?.map { it.trim() }
+        ?.filter { it.isNotEmpty() }
+        ?: emptyList()
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.neoforged.net/releases/") { name = "NeoForged" }
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie" }
+        maven("https://maven.kikugie.dev/releases") { name = "KikuGie Releases" }
+        maven("https://maven.parchmentmc.org") { name = "ParchmentMC" }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("dev.kikugie.stonecutter") version "0.7.10"
+}
+
+stonecutter {
+    create(rootProject) {
+        fun match(version: String, vararg loaders: String) = loaders
+                .forEach { vers("$version-$it", version).buildscript = "build.$it.gradle.kts" }
+
+        match("1.21.9", "fabric")
+
+        vcsVersion = "1.21.9-fabric"
+    }
+}

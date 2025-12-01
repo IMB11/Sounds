b74d338f929732e9eb7fa42031db66e64bad884b
Cassian Godsted <cassian@godsted.com>
2025-11-30 21:47:05 -0500

    fix: copper armor sounds

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("dev.kikugie.stonecutter")
     id("co.uzzu.dotenv.gradle") version "4.0.0"
-    id("fabric-loom") version "1.11-SNAPSHOT" apply false
+    id("fabric-loom") version "1.13-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.110" apply false
     id ("dev.kikugie.postprocess.jsonlang") version "2.1-beta.4" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false

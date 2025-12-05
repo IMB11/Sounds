12bb1b9e2935eed177cd9fc939ed15304458364f
Cassian Godsted <cassian@godsted.com>
2025-12-05 13:03:26 -0500

    fix: crash on neo servers
    Fixes #222

--- src/main/java/dev/imb11/sounds/mixin/compat/AmendmentsDirectionalCakeMixin.java
+++ src/main/java/dev/imb11/sounds/mixin/compat/AmendmentsDirectionalCakeMixin.java
@@ -10,11 +10,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Pseudo
 @Mixin(DirectionalCakeBlock.class)
 public class AmendmentsDirectionalCakeMixin {
     @Unique

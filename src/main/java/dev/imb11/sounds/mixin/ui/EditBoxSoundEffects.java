4bcae6317fcc6ed48ab1c5ec0402af020387b35d
Cassian Godsted <cassian@godsted.com>
2025-12-08 18:45:03 -0500

    feat: improved key typing

---
+++ src/main/java/dev/imb11/sounds/mixin/ui/EditBoxSoundEffects.java
@@ -0,0 +1,29 @@
+package dev.imb11.sounds.mixin.ui;
+
+import dev.imb11.sounds.config.SoundsConfig;
+import dev.imb11.sounds.config.UISoundsConfig;
+import dev.imb11.sounds.util.MixinStatics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Pseudo
+@Mixin(value = EditBox.class, remap = false)
+public abstract class EditBoxSoundEffects extends AbstractWidget {
+
+    public EditBoxSoundEffects(int i, int j, int k, int l, Component component) {
+        super(i, j, k, l, component);
+    }
+
+    @Inject(method = "keyPressed", at = @At("RETURN"), remap = false)
+    private void $item_picked_up(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        if (this.isActive() && this.isFocused() && MixinStatics.isNotSpecialKey(keyCode)) {
+            SoundsConfig.get(UISoundsConfig.class).inventoryTypingSoundEffect.playSound();
+        }
+    }
+}

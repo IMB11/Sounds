9735f56e812a16df8fd8e0706af19614e00620db
Cassian <cassian@godsted.com>
2026-02-16 17:18:46 -0500

    feat: allow disabling dynamic item sounds

--- src/main/java/dev/imb11/sounds/config/UISoundsConfig.java
+++ src/main/java/dev/imb11/sounds/config/UISoundsConfig.java
@@ -49,6 +49,8 @@ public class UISoundsConfig extends ConfigGroup<UISoundsConfig> implements YetAn
     public float itemSoundCooldown = 0.05f;
     @SerialEntry
     public boolean enableItemSoundCooldown = true;
+    @SerialEntry
+    public boolean enableDynamicItemSounds = true;
 
     public UISoundsConfig() {
         super(UISoundsConfig.class);
@@ -90,6 +92,7 @@ public class UISoundsConfig extends ConfigGroup<UISoundsConfig> implements YetAn
                 .build());
         builder.category(ConfigCategory.createBuilder()
                 .name(Component.translatable("sounds.config.ui.item_management"))
+                .option(HELPER.get("enableDynamicItemSounds", defaults.enableDynamicItemSounds, () -> config.enableDynamicItemSounds, v -> config.enableDynamicItemSounds = v))
                 .option(HELPER.get("ignoreEmptyInventorySlots", defaults.ignoreEmptyInventorySlots, () -> config.ignoreEmptyInventorySlots, v -> config.ignoreEmptyInventorySlots = v))
                 .option(HELPER.get("enableItemSoundCooldown", defaults.enableItemSoundCooldown, () -> config.enableItemSoundCooldown, v -> config.enableItemSoundCooldown = v))
                 .option(HELPER.getField("itemSoundCooldown", 0.0f, Float.MAX_VALUE, defaults.itemSoundCooldown, () -> config.itemSoundCooldown, v -> config.itemSoundCooldown = v))
--- src/main/java/dev/imb11/sounds/sound/context/ItemStackSoundContext.java
+++ src/main/java/dev/imb11/sounds/sound/context/ItemStackSoundContext.java
@@ -2,6 +2,8 @@ package dev.imb11.sounds.sound.context;
 
 import dev.imb11.sounds.api.SoundDefinition;
 import dev.imb11.sounds.api.context.DynamicSoundContext;
+import dev.imb11.sounds.config.SoundsConfig;
+import dev.imb11.sounds.config.UISoundsConfig;
 import dev.imb11.sounds.dynamic.DynamicSoundHelper;
 import dev.imb11.sounds.mixin.accessors.BlockAccessor;
 import net.minecraft.client.resources.sounds.SoundInstance;
@@ -30,29 +32,29 @@ public class ItemStackSoundContext implements DynamicSoundContext<ItemStack> {
 
     @Override
     public SoundInstance handleContext(ItemStack context, Identifier fallback, float pitch, float volume) {
-        var item = context.getItem();
+        if (SoundsConfig.get(UISoundsConfig.class).enableDynamicItemSounds) {
+            var item = context.getItem();
+            if (item instanceof BlockItem blockItem) {
+                var block = blockItem.getBlock();
+                fallback = this.blockSoundType.getTransformer().apply(((BlockAccessor)block).invokeGetSoundType(block.defaultBlockState()));
+            }
 
-        if (item instanceof BlockItem blockItem) {
-            var block = blockItem.getBlock();
-            fallback = this.blockSoundType.getTransformer().apply(((BlockAccessor)block).invokeGetSoundType(block.defaultBlockState()));
-        }
+            for (SoundDefinition<Item> definition : DynamicSoundHelper.<Item>getDefinitions("items")) {
+                if (definition.getKeys().isValid(item)) {
+                    fallback = definition.getSoundEvent();
 
-        for (SoundDefinition<Item> definition : DynamicSoundHelper.<Item>getDefinitions("items")) {
-            if (definition.getKeys().isValid(item)) {
-                fallback = definition.getSoundEvent();
+                    if (definition.getPitch().isPresent()) {
+                        pitch = definition.getPitch().get();
+                    }
 
-                if (definition.getPitch().isPresent()) {
-                    pitch = definition.getPitch().get();
+                    if (definition.getVolume().isPresent()) {
+                        volume = definition.getVolume().get();
+                    }
+
+                    break;
                 }
-
-                if (definition.getVolume().isPresent()) {
-                    volume = definition.getVolume().get();
-                }
-
-                break;
             }
         }
-
         return createSoundInstance(fallback, pitch, volume);
     }
 }

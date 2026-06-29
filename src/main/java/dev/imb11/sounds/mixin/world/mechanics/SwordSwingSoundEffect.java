51c52a5190ead554c0ba69d41fcd21dc963f0ee3
Cassian <cassian@godsted.com>
2026-06-29 17:26:37 -0400

    Fix uncustomizable swoosh sound

--- src/main/java/dev/imb11/sounds/mixin/world/mechanics/SwordSwingSoundEffect.java
+++ src/main/java/dev/imb11/sounds/mixin/world/mechanics/SwordSwingSoundEffect.java
@@ -1,33 +1,23 @@
 package dev.imb11.sounds.mixin.world.mechanics;
 
-import dev.imb11.sounds.api.config.ConfiguredSimpleSoundInstance;
 import dev.imb11.sounds.config.SoundsConfig;
 import dev.imb11.sounds.config.WorldSoundsConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.resources.sounds.SoundInstance;
-import net.minecraft.core.BlockPos;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class SwordSwingSoundEffect {
     @Shadow
     public int swingTime;
-    @Unique
-    public ConfiguredSimpleSoundInstance sounds$currentSwordSwooshSound;
     @Unique
     private static long lastPlayed = 0;
     @Unique
@@ -40,11 +30,8 @@ public class SwordSwingSoundEffect {
         if (!entity.level().isClientSide()) return;
         if (entity instanceof LocalPlayer localPlayer && Minecraft.getInstance().player != null && localPlayer.getUUID().equals(Minecraft.getInstance().player.getUUID())) {
             if (entity.getItemInHand(interactionHand).is(ItemTags.SWORDS) && (currentTime - lastPlayed > COOLDOWN)) {
-                this.sounds$currentSwordSwooshSound = SoundsConfig.get(WorldSoundsConfig.class).swordSwooshSoundEffect.getSoundInstance();
-                if (this.sounds$currentSwordSwooshSound != null) {
-                    SoundsConfig.get(WorldSoundsConfig.class).swordSwooshSoundEffect.playSound(this.sounds$currentSwordSwooshSound);
-                    lastPlayed = currentTime;
-                }
+                SoundsConfig.get(WorldSoundsConfig.class).swordSwooshSoundEffect.playSound();
+                lastPlayed = currentTime;
             }
         }
     }

381c2eecc8618caf4921f7b841e5a0fd6772db0a
Cassian <cassian@godsted.com>
2026-07-31 16:41:03 -0400

    Build against MRU rather than Yumi MC Foundation

--- src/main/java/dev/imb11/sounds/SoundsClient.java
+++ src/main/java/dev/imb11/sounds/SoundsClient.java
@@ -1,13 +1,12 @@
 package dev.imb11.sounds;
 
+import cc.cassian.mru.client.events.ClientRegisterEvent;
+import cc.cassian.mru.events.CommonRegisterEvent;
 import dev.imb11.mru.LoaderUtils;
 import dev.imb11.sounds.config.SoundsConfig;
 import dev.imb11.sounds.dynamic.DynamicSoundHelper;
 import dev.imb11.sounds.sound.CustomSounds;
 import dev.imb11.sounds.util.ConfigSetters;
-import dev.yumi.mc.core.api.ModContainer;
-import dev.yumi.mc.core.api.entrypoint.ModInitializer;
-import dev.yumi.mc.core.api.entrypoint.client.ClientModInitializer;
 import net.minecraft.util.RandomSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ import java.nio.file.Path;
 
 import net.minecraft.resources.Identifier;
 
-public class SoundsClient implements ClientModInitializer, ModInitializer {
+public class SoundsClient implements ClientRegisterEvent, CommonRegisterEvent {
     public static final RandomSource RANDOM = RandomSource.create();
     public static final Path DEFAULT_PACK_PATH = LoaderUtils.getConfigFolder("sounds").resolve("dynamic_sounds");
     public static final Logger LOGGER = LoggerFactory.getLogger("Sounds");
@@ -26,12 +25,12 @@ public class SoundsClient implements ClientModInitializer, ModInitializer {
     }
 
     @Override
-    public void onInitialize(ModContainer modContainer) {
+    public void onInitialize() {
         CustomSounds.initialize();
     }
 
     @Override
-    public void onInitializeClient(ModContainer modContainer) {
+    public void onInitializeClient() {
         DynamicSoundHelper.initialize();
         SoundsConfig.loadAll();
         ConfigSetters.init();
